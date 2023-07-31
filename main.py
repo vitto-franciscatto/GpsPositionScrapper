4435312dcaf4558cce9f3164c0faf5d5e01918f1
Vitto Franciscatto <vitto.franciscatto@gmail.com>
2023-07-30 23:15:16 -0300

    :sparkles: feat: add options for using firefox and headed operation

--- main.py
+++ main.py
@@ -2,38 +2,48 @@ import sys
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import getopt
+import time
 
 def scrap_gps_pos(argv):
-    url = ''
-    verbose = False
 
-    options, argv = getopt.getopt(argv, "u:v", ["url=", "verbose"])
+    url = ''
+    useFirefox = False
+    useHeadless = True
+
+    options, argv = getopt.getopt(argv, "u:v", ["url=", "use-firefox", "headed"])
     for opt, arg in options:
         if opt in ('-u', '--url'):
             url = arg
         if opt in ('-v', '--verbose'):
             verbose = True
+        if opt == '--use-firefox':
+            useFirefox = True
+        if opt == '--headed':
+            useHeadless = False
 
-    if verbose:
-        print(url)
+    if useFirefox:
+        options = Options()
+        if useHeadless:
+            options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless=new")
+    else:
+        chrome_options = webdriver.ChromeOptions()
+        if useHeadless:
+            chrome_options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=chrome_options)
 
-    chrome_driver = webdriver.Chrome(options=chrome_options)
-    chrome_driver.get(url)
+    driver.get(url)
+    time.sleep(1)
 
-    element = WebDriverWait(chrome_driver, 5).until(EC.text_to_be_present_in_element(By.CLASS_NAME, 'position_x'))
-
-    lat = chrome_driver.find_element(By.CLASS_NAME, 'position_x')
-    long = chrome_driver.find_element(By.CLASS_NAME, 'position_y')
-    height = chrome_driver.find_element(By.CLASS_NAME, 'position_h')
+    lat = driver.find_element(By.CLASS_NAME, 'position_x')
+    long = driver.find_element(By.CLASS_NAME, 'position_y')
+    height = driver.find_element(By.CLASS_NAME, 'position_h')
 
     print(f'LAT: {lat.text} | LONG: {long.text} | HGT: {height.text}')
-    chrome_driver.quit()
+    driver.quit()
 
 
 if __name__ == '__main__':
